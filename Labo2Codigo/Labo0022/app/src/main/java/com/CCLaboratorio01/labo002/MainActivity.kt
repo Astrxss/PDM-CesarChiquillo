35976f2736559fc4ec49939a1fd2895269bceb11
astrxss <00225424@uca.edu.sv>
2026-03-24 19:18:36 -0600

    FEAT: Laboratorio02

---
+++ Labo2Codigo/Labo0022/app/src/main/java/com/CCLaboratorio01/labo002/MainActivity.kt
@@ -0,0 +1,153 @@
+package com.CCLaboratorio01.labo002
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.CCLaboratorio01.labo002.ui.theme.Labo002Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Labo002Theme {
+                ListName()
+            }
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun ListName () {
+    val nombre: MutableState<String> = remember { mutableStateOf("") };
+    val listaNombres: MutableList<String> = remember { mutableStateListOf("") }
+
+    Column(
+        modifier = Modifier.
+        fillMaxSize().
+        padding(16.dp),
+        verticalArrangement = Arrangement.Top
+    ) {
+
+        TextField(
+            modifier = Modifier.
+            fillMaxWidth(),
+            value = nombre.value,
+            onValueChange = {
+
+                nombre.value = it
+            },
+            label = { Text("Nombre") }
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Button(
+                onClick = {
+                    if (nombre.value.isNotBlank()){
+                    listaNombres.add(nombre.value)
+                    nombre.value = ""}
+                }
+            ) {
+                Text(text = "Guardar")
+            }
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Row(
+            Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                modifier = Modifier.fillMaxWidth(0.5f),
+                text = "Listado de nombres y posiciones en lista"
+            )
+
+            Button(
+                onClick = {
+                    listaNombres.clear()
+                }
+            ) {
+                Text(text = "Limpiar")
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .border(
+                    width = 3.dp,
+                    color = Color.Blue,
+                    shape = RoundedCornerShape(12.dp)
+                )
+                .padding(16.dp)
+        ) {
+
+        LazyColumn(
+
+            modifier = Modifier.fillMaxSize()
+
+        ) {
+
+            itemsIndexed(listaNombres.toList()) { index, item ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = item
+                    )
+                    Text(
+                        text = (index + 1).toString()
+                    )
+                }
+
+            }
+        }
+        }
+
+
+    }
+}
